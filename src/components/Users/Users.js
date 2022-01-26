73e5024e571fbe05fbc9293b2f04b9fa506e4a28
jeremiahlee94 <jhwlee94@gmail.com>
2022-01-26 08:41:24 -0800

    Update create componenet and users component

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -11,19 +11,21 @@ function Users(props) {
     const [ posts, setPosts ] = useState([]);
 
     // useParams to hold the id of the user
+    // const { state } = useParams();
     const { user } = useParams();
 
     // useEffect to fetch all the posts of the user in a location
     useEffect(() => {
-        // getPosts();
+        getPosts();
     }, [user]);
 
     // async await for axios fetch request
     const getPosts = async () => {
         try {
             // change fetch request path to localhost:3001/posts/user
-            const res = await axios.get(`http://localhost:3001/:user`);
-            setPosts(res);
+            const res = await axios.get(`http://localhost:3001/api/posts/user/${user}`);
+            setPosts(res.data);
+            // console.log(res.data);
         } catch (error) {
             console.log(error);
         }
@@ -31,17 +33,22 @@ function Users(props) {
 
     return (
         <div>
-            hello from users
-            {posts ? (
+            {posts.length > 0 ? (
             <>
+                <h2>{user}</h2>
+                <h3>{posts[0].years_of_residence} years in {posts[0].state}</h3>
                 <div>
-                    {posts.map((post) => {
+                    {posts.map((post, index) => {
                         return (
-                            <>
+                            <div key={`${post.user.name}-${index}`}>
                                 <h2>{post.title}</h2>
+                                <h3>{post.city}</h3>
+                                <h3>{post.type}</h3>
                                 <h3>{post.date}</h3>
                                 <p>{post.message}</p>
-                            </>
+                                <button>Edit</button>
+                                <button>Delete</button>
+                            </div>
                         );
                     })}
                 </div>
