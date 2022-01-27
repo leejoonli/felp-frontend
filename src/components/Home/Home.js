ce4c2eac1958595a26dff9d31b82319022e0da9c
fcondori1 <frankcondori97@gmail.com>
2022-01-27 13:02:01 -0500

    useLocation functionality

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,9 @@
-import Navigation from '../Navigation/Navigation';
-
 // Dependencies
 import { Link } from 'react-router-dom';
 
 function Home(props) {
 	return (
 		<div>
-			<div>
-				<Navigation />
-			</div>
 			<h2><Link to='/NY'>New York</Link></h2>
 			<h2><Link to='/NJ'>New Jersey</Link></h2>
 			<h2><Link to='/PA'>Pennsylvania</Link></h2>
