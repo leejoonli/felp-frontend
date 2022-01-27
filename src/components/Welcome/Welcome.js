1317d6d6c0617c1b1f9b8fcd4845d9b52b8eef5f
thejustinlombardi <thejustinlombardi@gmail.com>
2022-01-27 17:54:07 -0500

    Complete 1st draft of Welcome page

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -36,10 +36,27 @@ function Welcome(props) {
 			</div>
 
 			<div className={styles.welcome_title_container}>
-				<h1 className={styles.welcome_title}>F E L P</h1>
-				<Link to="/home" className={styles.welcome_enter}>
-					<h3 className={styles.welcome_h3}>Click Here to Enter!</h3>
-				</Link>
+				<div>
+					<img
+						src="https://i.imgur.com/ogi8uwx.png"
+						alt="felp-logo"
+						className={styles.welcome_logo}
+					/>
+				</div>
+				<div className={styles.welcome_text}>
+					<h1 className={styles.welcome_title}>F E L P</h1>
+					<Link to="/home" className={styles.welcome_enter}>
+						<h3 className={styles.welcome_h3}>Click Here to Enter!</h3>
+					</Link>
+				</div>
+
+				<div>
+					<img
+						src="https://i.imgur.com/ogi8uwx.png"
+						alt="felp-logo"
+						className={styles.welcome_logo}
+					/>
+				</div>
 			</div>
 			<footer>
 				<div className={styles.footer_div}>
