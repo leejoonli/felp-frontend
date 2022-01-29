31a1e30a0ee660f80b0c76b6051e074c667090fc
fcondori1 <frankcondori97@gmail.com>
2022-01-28 19:06:20 -0500

    footer and nav update

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -10,8 +10,14 @@ function Navigation(props) {
 	return (
 		<div className={styles.nav}>
 			<h1 className={styles.nav_logo}>
-				{/* logo will go here  */}
-				<Link to='/Home'>Felp</Link>
+				<Link to='/Home'>
+					<img
+						className={styles.nav_logob}
+						src='https://i.imgur.com/k8S94K4.png
+'
+						alt='felp-logo'
+					/>
+				</Link>
 			</h1>
 			{/* <nav className={styles}>
 				<ul className={styles.nav_experience}>
@@ -20,12 +26,14 @@ function Navigation(props) {
 				</ul>
 			</nav> */}
 			<div className={styles.nav_img}>
-				<img
-					className={styles.nav_imgb}
-					src='https://img.icons8.com/windows/32/000000/menu--v3.png'
-					alt='dropdown'
-					onClick={() => setOpen(!open)}
-				/>
+				<div>
+					<img
+						className={styles.nav_imgb}
+						src='https://img.icons8.com/windows/32/000000/menu--v3.png'
+						alt='dropdown'
+						onClick={() => setOpen(!open)}
+					/>
+				</div>
 				{open && props.children}
 			</div>
 			<div id='root'></div>
